c568085760ffc809c947b9b464f4554f1a609b23
nisola <jimohayomide20@gmail.com>
2026-01-19 14:14:10 +0100

    added a trending features

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const btn = document.querySelector('.search button');
 const main_grid = document.querySelector('.favorite .movies-grid');
 const main_grid_title = document.querySelector('.favorite h1')
 
+const trending_el = document.querySelector('.trending .movies-grid')
+
 const popup_container = document.querySelector('.popup-container')
 
 function add_click_effect_to_card (cards) {
@@ -142,13 +144,112 @@ async function show_popup (card) {
     x_icon.addEventListener('click', () =>     popup_container.classList.remove('show-popup'));
     
     //to give functionality to the heart icon
-    const heart_icon = popup_container.querySelector('.heart-icon')
-    heart_icon.addEventListener('click', () => {
-        if(heart_icon.classList.contains('change-color')) {
-            heart_icon.classList.remove('change-color')
-        }
-        else {
-            heart_icon.classList.add('change-color')
-        }
-    })
+   const heart_icon = popup_container.querySelector('.heart-icon');
+
+// keep heart red if already in favorites
+if (get_LS().includes(movie_id)) {
+  heart_icon.classList.add('change-color');
 }
+
+heart_icon.addEventListener('click', () => {
+  if (heart_icon.classList.contains('change-color')) {
+    remove_LS(movie_id);
+  } else {
+    add_to_LS(movie_id);
+  }
+  heart_icon.classList.toggle('change-color');
+  fetch_favorite_movies();
+});
+
+  fetch_favorite_movies()
+}
+
+// to make the video show at the favorite secton when click on favorite icon, and want it to be saved even after we close the browser
+//we are going to use local storage for that
+//function for local storage
+function get_LS () {
+    const movie_ids = JSON.parse(localStorage.getItem('movie-id'))
+    return movie_ids === null ? [] : movie_ids //this means if there is not an id just return an empty array
+}
+function add_to_LS (id) {
+    const movie_ids = get_LS()
+     if (!movie_ids.includes(id))
+    localStorage.setItem('movie-id', JSON.stringify([...movie_ids, id]))
+}
+function remove_LS (id) {
+     const movie_ids = get_LS()
+     localStorage.setItem('movie-id', JSON.stringify(movie_ids.filter(e => e !== id)))
+}
+//to add movie to the favorite but then i will create a function that fetches the local storage everytime, add a movie and retunr it anytime
+fetch_favorite_movies()
+async function fetch_favorite_movies () {
+    main_grid.innerHTML = ''
+    const movies_LS = await get_LS()
+    const movies = []
+    for( let i = 0; i <= movies_LS.length - 1; i++) {
+        const movie_id = movies_LS[i]
+        let movie = await get_movies_by_id(movie_id);
+        add_favorites_to_dom_from_LS(movie)
+        movies.push(movie)
+    }
+}
+function  add_favorites_to_dom_from_LS(movie_data) {
+      main_grid.innerHTML += `
+           <div class="card" data-id="${movie_data.id}">
+                <div class="img">
+                <img src="${image_path + movie_data.poster_path}" alt="">
+                </div>
+                <div class="info">
+                    <h2>${movie_data.title}</h2>
+                    <div class="single-info">
+                        <span>Rate:</span>
+                        <span>${movie_data.vote_average}</span>
+                    </div>
+                    <div class="single-info">
+                        <span>Release Date:</span>
+                        <span>${movie_data.release_date}</span>
+                    </div>
+                </div>
+            </div>
+     `
+     const cards = document.querySelectorAll('.card')
+     add_click_effect_to_card(cards)
+}
+//adding the trending movies features
+get_trending_movies()
+async function get_trending_movies () {
+    const resp = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`) 
+    const respData = await resp.json()
+    return respData.results;
+    }
+add_to_dom_trending()
+    async function add_to_dom_trending () {
+
+        const data = await get_trending_movies()
+        console.log(data)
+       
+        //this will give us only 10 results
+        trending_el.innerHTML = data.slice(0, 20).map(e => {
+            return `
+             <div class="card" data-id="${e.id}">
+                <div class="img">
+                <img src="${image_path + e.poster_path}" alt="">
+                </div>
+                <div class="info">
+                    <h2>${e.title}</h2>
+                    <div class="single-info">
+                        <span>Rate:</span>
+                        <span>${e.vote_average}</span>
+                    </div>
+                    <div class="single-info">
+                        <span>Release Date:</span>
+                        <span>${e.release_date}</span>
+                    </div>
+                </div>
+            </div>
+            `
+        }). join('');
+        
+  const cards = trending_el.querySelectorAll('.card');
+  add_click_effect_to_card(cards);
+    }
